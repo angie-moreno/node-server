6f856c8d53c6249952bb909341f82154034d233b
angie <aymorenof783230@gmail.com>
2023-05-30 13:44:50 -0600

    servidor lista de tareas

--- lista-tareas.js
+++ lista-tareas.js
@@ -1,50 +1,33 @@
-const readline = require("readline-sync");
+const http = require("http");
+const port = 3000;
+const host = "localhost";
 
-const tasks = [];
-
-exports.addTask = () => {
-  const indicador = readline.question("Indicador de la tarea ");
-  const descripcion = readline.question("Descripción de la tarea ");
-
-  const task = {
-    indicador,
-    descripcion,
-    completada: false,
-  };
-
-  tasks.push(task);
-  console.log("Tarea agragada correctamente");
-};
-
-exports.deleteTask = () => {
-  const indicador = readline.question("Indicador de la tarea a eliminar");
-
-  const taskIndex = tasks.findIndex((task) => task.indicador == indicador);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-    console.log("La tarea fue eliminada correctamente");
+const server = http.createServer((req, res) => {
+  const tasks = [
+    {
+      id: 1,
+      description: "Hacer la compra",
+      completed: false,
+    },
+    {
+      id: 2,
+      description: "Lavar los platos",
+      completed: true,
+    },
+    {
+      id: 3,
+      description: "Sacar la basura",
+      completed: false,
+    },
+  ];
+  if (req.method === "GET" && req.url === "/tasks") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(tasks));
   } else {
-    console.log("No se encontro ninguna tarea con el indicador proporcionado");
+    res.statusCode = 404;
+    res.end("recurso no encontrado");
   }
-};
-
-exports.completeTask = () => {
-  const indicador = readline.question(
-    "Indicador de la tarea que quieres completar"
-  );
-
-  const task = tasks.find((task) => task.indicador == indicador);
-  if (task) {
-    task.completada = true;
-    console.log("La tarea ha sido completada");
-  } else {
-    console.log("No se encontro ninguna tarea con el indicador proporcionado");
-  }
-};
-exports.showTasks = () => {
-  console.log("Lista de tareas");
-  tasks.forEach((task) => {
-    const status = task.completada ? "completada" : "pendiente";
-    console.log(`${task.indicador} ${task.descripcion} ${status} `);
-  });
-};
+});
+server.listen(port, host, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
